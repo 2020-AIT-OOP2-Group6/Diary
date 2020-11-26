a71e636e3c65d7a3901f6596373e8c8b96949b6d
kensuke-b <75057491+kensuke-b@users.noreply.github.com>
2020-11-26 16:51:36 +0900

    全員動作確認済み

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from diaries.DiarySample import DiarySample
 from diaries.Toyama_diary import Toyama_diary
 from diaries.Iwama_diary import Iwama_diary
 from diaries.BabaDiary import BabaDiary
-from diaries.FugaDiaryNew import FugaDiaryNew
+from diaries.DiaryFugaNew import FugaDiaryNew
 from diaries.NishizawaDiary import NishizawaDiary
 
 
