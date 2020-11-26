a75d30093aa844c6661445f61c843a94da5408fb
happy663 <53854298+happy663@users.noreply.github.com>
2020-11-26 16:51:57 +0900

    ちゃんと動く

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from diaries.DiarySample import DiarySample
 from diaries.Toyama_diary import Toyama_diary
 from diaries.Iwama_diary import Iwama_diary
 from diaries.BabaDiary import BabaDiary
-from diaries.FugaDiaryNew import FugaDiaryNew
+from diaries.DiaryFugaNew import FugaDiaryNew
 from diaries.NishizawaDiary import NishizawaDiary
 
 
 diaries = [DiarySample(), BabaDiary(), FugaDiaryNew(),
-           Toyama_diary(),NishizawaDiary(), Iwama_diary()]
+           Toyama_diary(), NishizawaDiary(), Iwama_diary()]
 
 
 for d in diaries:
