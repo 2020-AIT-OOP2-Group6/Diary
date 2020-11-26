13c2e4caff38d0e4d9d567959e58f8ed82285ac4
r121212-Iwama <75057420+r121212-Iwama@users.noreply.github.com>
2020-11-26 16:25:44 +0900

    Diaryadd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from diaries.DiarySample import DiarySample
+from diaries.Toyama_diary import Toyama_diary
+from diaries.Iwama_diary import Iwama_diary
 
-diaries = [DiarySample(), ]
+diaries = [DiarySample(),Toyama_diary(), Iwama_diary() ]
 
 for d in diaries:
     print("---------------------------------")
